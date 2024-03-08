11bb004026eb5d2bd42a12122494a3bd4ec18760
minkxx <aryuokk@gmail.com>
2024-03-08 14:33:11 +0530

    add web

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home_page():
-    return "Bot Running"
+def hello_world():
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
